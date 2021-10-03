5b2b814b53e999a5ebaaa6dbab809176c48a2727
Paolo Eusebi <paoloeusebi@gmail.com>
2021-10-04 01:33:55 +0200

    removed data_prep

--- R/functions.R
+++ R/functions.R
@@ -1,3 +1,65 @@
+# Simulating 1 imperfect test with non-differential miss-classification ---
+sim_ve_1_imperfect_test <- function(n = 1000,
+                                    base_dis_prev = 0.05,
+                                    true_OR = 0.1,
+                                    covariates = F,
+                                    Se = 1,
+                                    Sp = 1) {
+  # generate 2 covariates
+  x_cont <- rnorm(n, mean = 0, sd = 1)
+  x_bin <- rbinom(n, size = 1, prob = 0.2)
+  x <- cbind(x_cont, x_bin)
+  x
+  
+  # Simulate propensity to be vaccinated
+  if (covariates == T) {vax.beta <- 0.5 + 0.25 * x_cont  - 0.25 * x_bin}
+  else if (covariates == F){vax.beta <- 0.5} 
+  
+  vax.prob <- exp(vax.beta) / (1 + exp(vax.beta))
+  vax      <- rbinom(n, 1, prob = vax.prob)
+  
+  # Simulate propensity to infections (vax + covariates related to no protection)
+  b0 = log(base_dis_prev/(1-base_dis_prev ))
+  if (covariates == T){dis.beta <- b0 + log(true_OR) * vax + 0.25 * x_cont  - 0.25 * x_bin}
+  else if (covariates == F) {dis.beta <- b0 + log(true_OR) * vax}
+  
+  dis.prob <- exp(dis.beta) / (1 + exp(dis.beta))
+  truestatus <- rbinom(n, 1, dis.prob)
+  # Disease diagnosed with imperfect test
+  dis <-
+    rbinom(n, 1, prob = truestatus * Se + (1 - truestatus) * (1 - Sp))
+  
+  df <- data.frame(truestatus, dis, vax, x)
+  
+  y <- rbind(as.numeric(table(df[df$vax==0,]$dis)), 
+             as.numeric(table(df[df$vax==1,]$dis))) %>% 
+    as.data.frame() %>%
+    select(V2, V1) %>%
+    as.matrix()
+  colnames(y) <- c("T+","T-")
+  rownames(y) <- c("V-","V+")
+  
+  t <- as.numeric(table(df$vax, df$dis))
+  
+  estimated_OR <- (t[1] * t[4]) / (t[2] * t[3])
+  
+  d <- list("estimated OR" = estimated_OR,
+            "true OR" = true_OR,
+            "data" = y)
+  
+  return(d)
+  
+}
+
+# tests
+sim <- sim_ve_1_imperfect_test(covariates = F, Sp=0.75, n=10000, true_OR = 0.1)
+sim$`estimated OR`
+sim$`true OR`
+sim$data
+
+
+# Simulating 2 imperfect tests with non-differential miss-classification --
+
 sim_ve_2_imperfect_tests <- function(n = 1000,
                                      base_dis_prev = 0.05,
                                      true_OR = 0.1,
@@ -41,25 +103,32 @@ sim_ve_2_imperfect_tests <- function(n = 1000,
   estimated_OR1 <- (t1[1] * t1[4]) / (t1[2] * t1[3])
   estimated_OR2 <- (t2[1] * t2[4]) / (t2[2] * t2[3])
   
+  y <- rbind(as.numeric(table(df[df$vax==0,]$dis1, df[df$vax==0,]$dis2)), 
+             as.numeric(table(df[df$vax==1,]$dis1, df[df$vax==1,]$dis2))) %>% 
+    as.data.frame() %>%
+    select(V4, V2, V3, V1) %>%
+    as.matrix()
+  colnames(y) <- c("T1+/T2+","T1+/T2-","T1-/T2+","T1-/T2-")
+  rownames(y) <- c("V+","V-")
+  
   d <- list("estimated OR T1" = estimated_OR1,
             "estimated OR T2" = estimated_OR2,
             "true OR" = true_OR,
-            "data" = df)
+            "data" = y)
   
   return(d)
   
 }
 
 # tests
-sim <- sim_ve_2_imperfect_tests(covariates = F, Sp2=0.75, n=10000, true_OR = 0.1)
+sim <- sim_ve_2_imperfect_tests(covariates = F, Se1=0.8, Sp1=0.95, Sp2=0.9, n=10000, true_OR = 0.2)
 sim$`estimated OR T1`
 sim$`estimated OR T2`
 sim$`true OR`
-# sim_ve_imperfect_tests(covariates = T)
-# sim_ve_imperfect_tests(covariates = F, n=10000000)
-# sim_ve_imperfect_tests(covariates = F, Sp2=0.75, n=10000000)
+sim$data
 
 
+# Simulating 2 imperfect tests with differential miss-classification ------
 
 sim_ve_imperfect_tests_diff <- function(n = 1000,
                                    base_dis_prev = 0.05,
@@ -109,10 +178,18 @@ sim_ve_imperfect_tests_diff <- function(n = 1000,
   estimated_OR1 <- (t1[1] * t1[4]) / (t1[2] * t1[3])
   estimated_OR2 <- (t2[1] * t2[4]) / (t2[2] * t2[3])
   
+  y <- rbind(as.numeric(table(df[df$vax==0,]$dis1, df[df$vax==0,]$dis2)), 
+             as.numeric(table(df[df$vax==1,]$dis1, df[df$vax==1,]$dis2))) %>% 
+    as.data.frame() %>%
+    select(V4, V2, V3, V1) %>%
+    as.matrix()
+  colnames(y) <- c("T1+/T2+","T1+/T2-","T1-/T2+","T1-/T2-")
+  rownames(y) <- c("V+","V-")
+  
   d <- list("estimated OR T1" = estimated_OR1,
             "estimated OR T2" = estimated_OR2,
             "true OR" = true_OR,
-            "data" = df)
+            "data" = y)
   
   return(d)
   
@@ -123,72 +200,4 @@ sim <- sim_ve_imperfect_tests_diff(covariates = F, Sp2_nV=0.75, n=10000, true_OR
 sim$`estimated OR T1`
 sim$`estimated OR T2`
 sim$`true OR`
-# sim_ve_imperfect_tests(covariates = T)
-# sim_ve_imperfect_tests(covariates = F, n=10000000)
-# sim_ve_imperfect_tests(covariates = F, Sp2=0.75, n=10000000)
-
-data_prep <- function(data){
-  df <- d$data
-  a <- as.numeric(table(df[df$vax==0,]$dis1, df[df$vax==0,]$dis2))
-  b <- as.numeric(table(df[df$vax==1,]$dis1, df[df$vax==1,]$dis2))
-  y <- rbind(a, b) %>% 
-    as.data.frame %>%
-    select(V4, V2, V3, V1) %>%
-    as.matrix()
-  colnames(y) <- c("T1+/T2+","T1+/T2-","T1-/T2+","T1-/T2-")
-  rownames(y) <- c("V+","V-")
-  return(y)
-}
-
-sim_ve_1_imperfect_test <- function(n = 1000,
-                                     base_dis_prev = 0.05,
-                                     true_OR = 0.1,
-                                     covariates = F,
-                                     Se = 1,
-                                     Sp = 1) {
-  
-  # generate 2 covariates
-  x_cont <- rnorm(n, mean = 0, sd = 1)
-  x_bin <- rbinom(n, size = 1, prob = 0.2)
-  x <- cbind(x_cont, x_bin)
-  x
-  
-  # Simulate propensity to be vaccinated
-  if (covariates == T) {vax.beta <- 0.5 + 0.25 * x_cont  - 0.25 * x_bin}
-  else if (covariates == F){vax.beta <- 0.5} 
-  
-  vax.prob <- exp(vax.beta) / (1 + exp(vax.beta))
-  vax      <- rbinom(n, 1, prob = vax.prob)
-  
-  # Simulate propensity to infections (vax + covariates related to no protection)
-  b0 = log(base_dis_prev/(1-base_dis_prev ))
-  if (covariates == T){dis.beta <- b0 + log(true_OR) * vax + 0.25 * x_cont  - 0.25 * x_bin}
-  else if (covariates == F) {dis.beta <- b0 + log(true_OR) * vax}
-  
-  dis.prob <- exp(dis.beta) / (1 + exp(dis.beta))
-  truestatus <- rbinom(n, 1, dis.prob)
-  # Disease diagnosed with imperfect test
-  dis <-
-    rbinom(n, 1, prob = truestatus * Se + (1 - truestatus) * (1 - Sp))
-   
-  df <- data.frame(truestatus, dis, vax, x)
-  
-  t <- as.numeric(table(df$vax, df$dis))
-  
-  estimated_OR <- (t[1] * t[4]) / (t[2] * t[3])
-  
-  d <- list("estimated OR" = estimated_OR,
-            "true OR" = true_OR,
-            "data" = df)
-  
-  return(d)
-  
-}
-
-# tests
-sim <- sim_ve_1_imperfect_test(covariates = F, Sp=0.75, n=10000, true_OR = 0.1)
-sim$`estimated OR`
-sim$`true OR`
-# sim_ve_imperfect_tests(covariates = T)
-# sim_ve_imperfect_tests(covariates = F, n=10000000)
-# sim_ve_imperfect_tests(covariates = F, Sp2=0.75, n=10000000)
+sim$data
