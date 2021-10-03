52e69380df121e8d78026a9081b3edede83dc562
Paolo Eusebi <paoloeusebi@gmail.com>
2021-10-04 01:34:17 +0200

    HP coded

--- R/models.R
+++ R/models.R
@@ -10,12 +10,39 @@ for (i in 1:m) {
                 pi[i] ~ dbeta(1,1)
                 }
 
-Se~dbeta(75,25)
-Sp~dbeta(90,10)
+Se~dbeta(HPSe[1], HPSe[2])
+Sp~dbeta(HPSp[1], HPSp[2])
 
 OR = (pi[2]/(1-pi[2])) / (pi[1]/(1-pi[1]))
 
-#data# m, n, y
+#data# m, n, y, HPSe, HPSp
+#inits#
+#monitor# Se, Sp, pi, OR
+}
+"
+blcm_2test_nondif <- " model {
+
+for (i in 1:m) {
+
+                # likelihood
+
+                y[i,1:4] ~ dmulti(prob[i,1:4],n[i])
+                prob[i,1] <- pi[i]*(Se[1]*Se[2]) + (1-pi[i])*((1-Sp[1])*(1-Sp[2]))
+                prob[i,2] <- pi[i]*(Se[1]*(1-Se[2])) + (1-pi[i])*((1-Sp[1])*Sp[2])
+                prob[i,3] <- pi[i]*((1-Se[1])*Se[2]) + (1-pi[i])*(Sp[1]*(1-Sp[2]))
+                prob[i,4] <- pi[i]*((1-Se[1])*(1-Se[2])) + (1-pi[i])*(Sp[1]*Sp[2])
+                # priors for prevalence parameters
+                pi[i] ~ dbeta(1,1)
+                }
+
+Se[1] ~ dbeta(HPSe[1,1], HPSe[1,1])T(1-Sp[1], )
+Sp[1] ~ dbeta(HPSp[1,1], HPSp[1,1])
+Se[2] ~ dbeta(HPSe[2,1], HPSe[2,2])T(1-Sp[2], )
+Sp[2] ~ dbeta(HPSp[2,1], HPSp[2,1])
+
+OR = (pi[2]/(1-pi[2])) / (pi[1]/(1-pi[1]))
+
+#data# m, n, y, HPSe, HPSp
 #inits#
 #monitor# Se, Sp, pi, OR
 }
@@ -42,49 +69,21 @@ blcm_2test_dif <- " model {
                 pi[2] ~ dbeta(1,1)
 
 
-Se_V[1] ~ dbeta(1, 1)T(1-Sp_V[1], )
-Sp_V[1] ~ dbeta(1,1)
-Se_V[2] ~ dbeta(1, 1)T(1-Sp_V[2], )
-Sp_V[2] ~ dbeta(1,1)
+Se_V[1] ~ dbeta(HPSe_V[1,1], HPSe_V[1,2])T(1-Sp_V[1], )
+Sp_V[1] ~ dbeta(HPSp_V[1,1], HPSp_V[1,2])
+Se_V[2] ~ dbeta(HPSe_V[2,1], HPSe_V[2,2])T(1-Sp_V[2], )
+Sp_V[2] ~ dbeta(HPSp_V[2,1], HPSe_V[2,2])
 
-Se_nV[1] ~ dbeta(1, 1)T(1-Sp_nV[1], )
-Sp_nV[1] ~ dbeta(1,1)
-Se_nV[2] ~ dbeta(1, 1)T(1-Sp_nV[2], )
-Sp_nV[2] ~ dbeta(1,1)
+Se_nV[1] ~ dbeta(HPSe_nV[1,1], HPSe_nV[1,2])T(1-Sp_nV[1], )
+Sp_nV[1] ~ dbeta(HPSp_nV[1,1], HPSp_nV[1,2])
+Se_nV[2] ~ dbeta(HPSe_nV[2,1], HPSe_nV[2,2])T(1-Sp_nV[2], )
+Sp_nV[2] ~ dbeta(HPSp_nV[2,1], HPSe_nV[2,2])
 
 OR = (pi[2]/(1-pi[2])) / (pi[1]/(1-pi[1]))
 
-#data# m, n, y
+#data# m, n, y, HPSe_V, HPSp_V, HPSe_nV, HPSp_nV
 #inits#
 #monitor# Se_V, Sp_V, Se_nV, Sp_nV, pi, OR
 }
 "
 
-blcm_2test_nondif <- " model {
-
-for (i in 1:m) {
-
-                # likelihood
-
-                y[i,1:4] ~ dmulti(prob[i,1:4],n[i])
-                prob[i,1] <- pi[i]*(Se[1]*Se[2]) + (1-pi[i])*((1-Sp[1])*(1-Sp[2]))
-                prob[i,2] <- pi[i]*(Se[1]*(1-Se[2])) + (1-pi[i])*((1-Sp[1])*Sp[2])
-                prob[i,3] <- pi[i]*((1-Se[1])*Se[2]) + (1-pi[i])*(Sp[1]*(1-Sp[2]))
-                prob[i,4] <- pi[i]*((1-Se[1])*(1-Se[2])) + (1-pi[i])*(Sp[1]*Sp[2])
-                # priors for prevalence parameters
-                pi[i] ~ dbeta(1,1)
-                }
-
-Se[1] ~ dbeta(1, 1)T(1-Sp[1], )
-Sp[1] ~ dbeta(1,1)
-Se[2] ~ dbeta(1, 1)T(1-Sp[2], )
-Sp[2] ~ dbeta(1,1)
-
-OR = (pi[2]/(1-pi[2])) / (pi[1]/(1-pi[1]))
-
-#data# m, n, y
-#inits#
-#monitor# Se, Sp, pi, OR
-}
-
-"
