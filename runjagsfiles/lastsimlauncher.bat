99a8d903801b2a6283c23c315a865ff0e773b3e3
Paolo Eusebi <paoloeusebi@gmail.com>
2021-10-08 23:36:57 +0200

    del

--- runjagsfiles/lastsimlauncher.bat
+++
@@ -1,3 +0,0 @@
-"C:/Users/E619545/AppData/Local/JAGS/JAGS-4.3.0/x64/bin/jags-terminal.exe" < sim.3/script.cmd > sim.3/jagsoutput.txt 2>&1 
-"C:/Users/E619545/OneDrive - UCB/Documents/R/R-4.0.2/library/runjags/xgrid/beep.bat"
- 
