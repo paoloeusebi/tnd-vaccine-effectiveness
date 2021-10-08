4327f7eff3f4f08d785f3f8745b3f116642ae114
paoloeusebi <paoloeusebi@gmail.com>
2021-10-08 22:08:33 +0000

    aaa

--- R/main.R
+++ R/main.R
@@ -1,4 +1,8 @@
 # load packages -----------------------------------------------------------
+list.of.packages <- c("tidyverse", "runjags", "rjags", "rootSolve")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+
 library(tidyverse)
 library(runjags)
 library(rjags)
