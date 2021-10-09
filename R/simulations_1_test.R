730c484db7e786dc5035de11f83bf799d19b2c25
Paolo Eusebi <paoloeusebi@gmail.com>
2021-10-10 00:51:37 +0200

    nsim = 100

--- R/simulations_1_test.R
+++ R/simulations_1_test.R
@@ -3,8 +3,8 @@ inits1 = list(".RNG.name" ="base::Mersenne-Twister", ".RNG.seed" = 100022)
 inits2 = list(".RNG.name" ="base::Mersenne-Twister", ".RNG.seed" = 300022)
 inits3 = list(".RNG.name" ="base::Mersenne-Twister", ".RNG.seed" = 500022)
 
-# n=10000; p1=0.01; Se=0.75; Sp = 0.75; true_OR=0.25
-n_sim <- 1000 # number of simulations
+# n=10000; p1=0.01; Se=0.75; Sp = 0.75; true_OR=0.25 for debugging
+n_sim <- 100 # number of simulations
 
 
 res <- NULL # initialization 
@@ -36,7 +36,7 @@ for (n in c(1000, 10000)){
         results <- run.jags(
           bm_1test_nondif,
           inits = list(inits1, inits2, inits3),
-          progress.bar='none'
+          progress.bar='none', silent.jags = T
         )
         
         res_i <- round(results$summaries, 3) %>% 
@@ -45,7 +45,6 @@ for (n in c(1000, 10000)){
           cbind(n, p1, Se, Sp, true_OR, raw_OR=d$`estimated OR`, n_scenario=j, n_simulation=i) %>%
           rename(parameter=rowname)
         
-          
         res <- rbind(res, res_i)
         
         print(paste("Simulation",i,"- Scenario", j))
@@ -57,3 +56,47 @@ for (n in c(1000, 10000)){
 }
 
 write.csv(res, "tfls/simulations_1_test.csv", row.names = FALSE)
+
+res <- read.csv("tfls/simulations_1_test.csv")
+
+res1 <- res %>%
+  as.tibble() %>%
+  filter(parameter=="OR") %>%
+  mutate(bias_OR1 = ((Median/true_OR)-1)*100,
+         bias_OR2 = ((raw_OR/true_OR)-1)*100) %>%
+  group_by(n_scenario, n, Se, Sp, p1, true_OR) %>%
+  summarize(bias_OR1_median = median(bias_OR1),
+            bias_OR2_median = median(bias_OR2))
+res1
+write.csv(res1, "tfls/simulations_1_test_summary.csv", row.names = FALSE)
+
+res2 <- res %>%
+  as.tibble() %>%
+  filter(parameter=="OR") %>%
+  mutate(bias_OR1 = ((Median/true_OR)-1)*100,
+         bias_OR2 = ((raw_OR/true_OR)-1)*100) %>%
+  pivot_longer(bias_OR1:bias_OR2, names_to = "bias_OR", values_to = "bias") %>%
+  mutate(Se=paste("Se=", Se),
+         Sp=paste("Sp=", Sp),
+         true_OR=paste("OR=", true_OR),
+         n=paste("n=", n))
+
+p <- ggplot(data = res2[res2$p1==0.025,],
+       aes(x=bias_OR, y=bias)) +
+  geom_boxplot() +
+  facet_grid(true_OR + n ~ Se + Sp,
+             scales="free_y")
+png("tfls/simulations_1_test_boxplot_p1_025.png", 
+    width = 480, height = 480)
+print(p)
+dev.off()
+
+p <- ggplot(data = res2[res2$p1==0.05,],
+       aes(x=bias_OR, y=bias)) +
+  geom_boxplot() +
+  facet_grid(true_OR + n ~ Se + Sp,
+             scales="free_y")
+png("tfls/simulations_1_test_boxplot_p1_050.png", 
+    width = 480, height = 480)
+print(p)
+dev.off()
