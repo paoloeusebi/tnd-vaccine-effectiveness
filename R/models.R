f618b9d0465fdcf121a8bbeaba8257b832b2e4cf
Paolo Eusebi <paoloeusebi@gmail.com>
2021-10-10 00:50:34 +0200

    N for sample size

--- R/models.R
+++ R/models.R
@@ -20,13 +20,15 @@ OR = (pi[2]/(1-pi[2])) / (pi[1]/(1-pi[1]))
 #monitor# Se, Sp, pi, OR
 }
 "
+
+
 blcm_2test_nondif <- " model {
 
 for (i in 1:m) {
 
                 # likelihood
 
-                y[i,1:4] ~ dmulti(prob[i,1:4],n[i])
+                y[i,1:4] ~ dmulti(prob[i,1:4], N[i])
                 prob[i,1] <- pi[i]*(Se[1]*Se[2]) + (1-pi[i])*((1-Sp[1])*(1-Sp[2]))
                 prob[i,2] <- pi[i]*(Se[1]*(1-Se[2])) + (1-pi[i])*((1-Sp[1])*Sp[2])
                 prob[i,3] <- pi[i]*((1-Se[1])*Se[2]) + (1-pi[i])*(Sp[1]*(1-Sp[2]))
@@ -35,14 +37,14 @@ for (i in 1:m) {
                 pi[i] ~ dbeta(1,1)
                 }
 
-Se[1] ~ dbeta(HPSe[1,1], HPSe[1,1])T(1-Sp[1], )
-Sp[1] ~ dbeta(HPSp[1,1], HPSp[1,1])
+Se[1] ~ dbeta(HPSe[1,1], HPSe[1,2])T(1-Sp[1], )
+Sp[1] ~ dbeta(HPSp[1,1], HPSp[1,2])
 Se[2] ~ dbeta(HPSe[2,1], HPSe[2,2])T(1-Sp[2], )
-Sp[2] ~ dbeta(HPSp[2,1], HPSp[2,1])
+Sp[2] ~ dbeta(HPSp[2,1], HPSp[2,2])
 
 OR = (pi[2]/(1-pi[2])) / (pi[1]/(1-pi[1]))
 
-#data# m, n, y, HPSe, HPSp
+#data# m, N, y, HPSe, HPSp
 #inits#
 #monitor# Se, Sp, pi, OR
 }
